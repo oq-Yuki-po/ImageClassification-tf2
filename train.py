e741af5479971668ed47d5ba27fcf5cec06d097e
oq-Yuki-op <o.yukinko0715@gmail.com>
2021-04-01 09:52:39 +0000

    add tfrecord training code

--- train.py
+++ train.py
@@ -1,14 +1,16 @@
-
 import tensorflow as tf
-import tensorflow_datasets as tfds
 import typer
 from tensorflow.keras import layers
 
-from modules.datasets import load_datasets
+from modules.datasets import load_tfrecords
 from modules.utils import load_yaml
+
+AUTOTUNE = tf.data.experimental.AUTOTUNE
 
 
 def main(config_path: str = 'config.yaml'):
+
+    typer.secho("----- start training -----", fg=typer.colors.GREEN)
 
     # load config
     config = load_yaml('config.yaml')
@@ -16,9 +18,20 @@ def main(config_path: str = 'config.yaml'):
     typer.secho(f"loaded config : {config_path}", fg=typer.colors.GREEN)
 
     # load dataset
-    typer.secho(f"loaded dataset : {config['dataset']}", fg=typer.colors.GREEN)
+    typer.secho(f"loaded train dataset : {config['train_data']}", fg=typer.colors.GREEN)
+    typer.secho(f"loaded test dataset : {config['test_data']}", fg=typer.colors.GREEN)
 
-    train_data, test_data = load_datasets(config)
+    train_ds = load_tfrecords(config['train_data'])
+    test_ds = load_tfrecords(config['test_data'])
+
+    train_ds = (train_ds.shuffle(buffer_size=config['train_image_count'])
+                .repeat()
+                .batch(config['batch_size'])
+                .prefetch(buffer_size=AUTOTUNE))
+
+    test_ds = (test_ds.repeat()
+               .batch(config['batch_size'])
+               .prefetch(buffer_size=AUTOTUNE))
 
     input_shape = (config['input_width'], config['input_height'], config['input_chanel'])
 
@@ -30,13 +43,21 @@ def main(config_path: str = 'config.yaml'):
     model.add(layers.Conv2D(64, (3, 3), activation='relu'))
     model.add(layers.Flatten())
     model.add(layers.Dense(64, activation='relu'))
-    model.add(layers.Dense(101, activation='softmax'))
+    model.add(layers.Dense(config['num_of_classes'], activation='softmax'))
 
     model.compile(optimizer='adam',
                   loss='sparse_categorical_crossentropy',
                   metrics=['accuracy'])
 
-    model.fit(train_data, epochs=5)
+    model.summary()
+
+    model.fit(train_ds,
+              steps_per_epoch=config['train_image_count'] // config['batch_size'],
+              validation_data=test_ds,
+              validation_steps=config['test_image_count'] // config['batch_size'],
+              epochs=config['epochs'])
+
+    typer.secho("----- end training -----", fg=typer.colors.GREEN)
 
 
 if __name__ == '__main__':
